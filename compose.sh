ca21e0dc88cc9d9ed540994a640bccc90399922d
Alex <format37@gmail.com>
2025-05-19 23:26:31 +0400

    compose fixed

--- compose.sh
+++ compose.sh
@@ -8,14 +8,5 @@ else
     echo "CONTAINER_NAME is set to $CONTAINER_NAME"
 fi
 
-# Notify and exit if data/config.json is not present
-if [ ! -f "data/config.json" ]; then
-    echo "Error: data/config.json is not present"
-    exit 1
-fi
-
-# sudo docker compose down -v
-# sudo docker compose up --build --force-recreate --remove-orphans -d
-
 sudo docker rm -fv "$CONTAINER_NAME" || true
 sudo docker compose up --build --remove-orphans -d --force-recreate $CONTAINER_NAME
